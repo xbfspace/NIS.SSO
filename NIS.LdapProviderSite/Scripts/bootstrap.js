1d9fd8e8ecd4577ef729b19a00a21079f84b94d4
xbfspace <314752497@qq.com>
2017-04-14 16:42:58 +0800

    f

--- NIS.LdapProviderSite/Scripts/bootstrap.js
+++ NIS.LdapProviderSite/Scripts/bootstrap.js
@@ -14,7 +14,7 @@
  * NUGET: END LICENSE TEXT */
 
 /**
-* bootstrap.js v3.0.0 by @fat and @mdo
+* bootstrap.js v3.0.0 by @fat and @mdo  
 * Copyright 2013 Twitter Inc.
 * http://www.apache.org/licenses/LICENSE-2.0
 */
